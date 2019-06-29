2c8b918276936f2fc45a53b5b639a8cf666699f2
beatdjam <beat3526@gmail.com>
2019-06-29 16:06:58 +0900

    [modify]make interface

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,11 +1,77 @@
 <template>
-  <h1>Hello World</h1>
+  <v-layout row >
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-toolbar color="light-blue" dark>
+          <v-toolbar-title>Todo</v-toolbar-title>
+        </v-toolbar>
+        <v-spacer/>
+        <v-form
+          ref="form"
+          lazy-validation
+        >
+          <v-text-field
+            @click:append=""
+            append-icon="edit"
+            label="Put Your Todo!"
+            single-line
+            outline
+            hide-details
+          />
+          <v-btn icon ripple>
+            <v-icon color="grey lighten-1">event</v-icon>
+          </v-btn>
+          <v-btn icon ripple>
+            <v-icon color="grey lighten-1">folder</v-icon>
+          </v-btn>
+          <v-btn icon ripple>
+            <v-icon color="grey lighten-1">account_circle</v-icon>
+          </v-btn>
+          <span class="text-sm-left">takeshi</span>
+        </v-form>
+        <v-list two-line subheader>
+          <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+            avatar
+            @click=""
+          >
+            <v-list-tile-action>
+              <v-checkbox v-model="sound"/>
+            </v-list-tile-action>
+            <v-list-tile-avatar>
+              <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-list-tile-action-text>{{ item.asignee }}</v-list-tile-action-text>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">delete</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
   export default {
-    data: () => ({
-    })
+    data () {
+      return {
+        items: [
+          { icon: 'folder', iconClass: 'red', title: 'Photos', subtitle: 'Jan 9, 2014', asignee : 'takeshi' },
+          { icon: 'folder', iconClass: 'indigo', title: 'Recipes', subtitle: 'Jan 17, 2014', asignee : 'takeshi' },
+          { icon: 'folder', iconClass: '#123456', title: 'Work', subtitle: 'Jan 28, 2014', asignee : 'takeshi' }
+        ]
+      }
+    }
   }
 </script>
 
